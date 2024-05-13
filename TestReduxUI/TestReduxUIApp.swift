1a2bb0c682b30a95bf1cd3f654ce7d5c69fdc489
Kim <nordinsane@gmail.com>
2024-05-13 22:38:47 +0200

    Initial Commit

---
+++ TestReduxUI/TestReduxUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReduxUIApp.swift
+//  TestReduxUI
+//
+//  Created by Kim Nordin on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct TestReduxUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
